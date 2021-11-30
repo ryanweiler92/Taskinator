499a93c9d0322684c0ee0213062fd133058e3d17
ryanweiler92 <ryanweiler92@gmail.com>
2021-11-30 09:49:06 -0500

    added delete function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var pageContentEl = document.querySelector("#page-content");
 var taskIdCounter =0;
 
 var formEl = document.querySelector("#task-form");
@@ -88,4 +89,21 @@ var createTaskActions = function(taskId) {
 
 
 formEl.addEventListener("submit", taskFormHandler);
+
+var taskButtonHandler = function(event) {
+    console.log(event.target);
+
+    if (event.target.matches(".delete-btn")) {
+
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" +  taskId + "']")
+    taskSelected.remove();
+}
+
+pageContentEl.addEventListener("click", taskButtonHandler);
 
